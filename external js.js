1f41af06e91015c5892b5ccd9832da9fcbac38ef
akshithbilla29 <126647081+akshithbilla29@users.noreply.github.com>
2023-03-30 21:22:50 +0530

    Add files via upload

---
+++ external js.js
@@ -0,0 +1,10 @@
+
+            function myfunction(){
+                document.getElementById("demo").innerHTML="para changed";
+            }
+             
+        
+        /* external.js
+function myExternalFunction() {
+    document.getElementById("demo").innerHTML = "This paragraph was changed by an external script.";
+}*/
